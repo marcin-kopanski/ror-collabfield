dbdb24a2c2c99d318c1e15bc4c6b5ddf8b1223d2
Marcin Kopański <marcin.kopanski@gmail.com>
2017-12-19 11:17:20 +0100

    Create a show template for posts
    - Add a show action and query a post to an instance variable
    - Create a show.scss file and add CSS

--- app/assets/javascripts/posts/modal.js
+++ app/assets/javascripts/posts/modal.js
@@ -1,4 +1,5 @@
 $(document).on('turbolinks:load', function() {
+  $('.myModal').modal('hide');
   // when a post is clicked, show its full content in a modal window
   $("body").on( "click", ".single-post-card, .single-post-list", function() {
     var posted_by = $(this).find('.post-content .posted-by').html();
@@ -10,5 +11,9 @@ $(document).on('turbolinks:load', function() {
     $('.loaded-data p').text(post_content);
     $('.loaded-data .interested a').attr('href', interested);
     $('.myModal').modal('show');
+  });
+
+  $("body").on( "click", ".interested a", function() {
+    $('.myModal').modal('hide');
   });
 });
