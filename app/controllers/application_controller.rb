1a402b845f5589dca8450ed4a735cc68521e2a3e
Marcin Kopański <marcin.kopanski@gmail.com>
2017-12-23 15:27:44 +0100

    - Create an OrderConversationsService and add specs for it - Define an all_by_user scope inside the Private::Conversation   model and add specs for it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :opened_conversations_windows
+  before_action :all_ordered_conversations
 
   def redirect_if_not_signed_in
     redirect_to root_path if !user_signed_in?
@@ -21,6 +22,12 @@ class ApplicationController < ActionController::Base
                                         .find(session[:private_conversations])
     else
       @private_conversations_windows = []
+    end
+  end
+
+  def all_ordered_conversations
+    if user_signed_in?
+      @all_conversations = OrderConversationsService.new({user: current_user}).call
     end
   end
 
